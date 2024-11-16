aed54c3f71473c438df0fb6b66d45f6f305e8b3e
Atinderpaul Kanwar <ap_kanwar@outlook.com>
2024-11-15 21:46:11 -0500

    Added Google tag

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,5 +1,6 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 import Script from 'next/script'
+import { GoogleTagManager } from '@next/third-parties/google'
 
 export default function Document() {
   return (
@@ -7,6 +8,7 @@ export default function Document() {
       <Head>
         <link rel="Shortcut Icon" href="logo.png" />
       </Head>
+      <GoogleTagManager gtmId="G-D4DH8C5BXL" />
       <body className="bg-dMode">
         <Main />
         <NextScript />

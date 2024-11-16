51f6b392303673e8040a35ba8eb5b2c8e4e74936
Atinderpaul Kanwar <ap_kanwar@outlook.com>
2024-11-15 21:59:31 -0500

    Added Google tag

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,6 +7,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="Shortcut Icon" href="logo.png" />
+        <meta name="google-site-verification" content="UL6ufNflon0lTU1RFa4byLOk_3eG6eoNPz-vnFyLh8w" />
         <script
           dangerouslySetInnerHTML={{
             __html: `

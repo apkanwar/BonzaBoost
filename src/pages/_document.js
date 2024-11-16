f252293b984b54089e330c442abf5359ff528581
Atinderpaul Kanwar <ap_kanwar@outlook.com>
2024-11-15 21:53:57 -0500

    Added Google tag

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,12 +7,31 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="Shortcut Icon" href="logo.png" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id=%27+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','G-D4DH8C5BXL');
+            `,
+          }}
+        />
       </Head>
       <GoogleTagManager gtmId="G-D4DH8C5BXL" />
       <body className="bg-dMode">
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `
+              <iframe src="https://www.googletagmanager.com/ns.html?id=G-D4DH8C5BXL"
+              height="0" width="0" style="display:none;visibility:hidden"></iframe>
+            `,
+          }}
+        />
         <Main />
         <NextScript />
-        <Script src="https://platform.billgang.com/embed.js" strategy="beforeInteractive"/>
+        <Script src="https://platform.billgang.com/embed.js" strategy="beforeInteractive" />
       </body>
     </Html>
   )

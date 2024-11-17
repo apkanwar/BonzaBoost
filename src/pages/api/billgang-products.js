a6368c173c7e8904e1dabce83dc3a9c378f3727f
Atinderpaul Kanwar <ap_kanwar@outlook.com>
2024-11-17 01:30:59 -0500

    await added

--- src/pages/api/billgang-products.js
+++ src/pages/api/billgang-products.js
@@ -1,9 +1,8 @@
+// api/billgang-products.js
+
 export default async function handler(req, res) {
     const { categoryId = 1 } = req.query;
-    console.log("API request received with:", req.method, "categoryId:", categoryId);
-
-    const url = `https://pg-api.billgang.com/v1/dash/shops/3a31d58c-9273-4d74-bb78-2c55aad39453/products?categoryId=${categoryId}`;
-    console.log("Fetching URL:", url);
+    const url = new URL(`https://pg-api.billgang.com/v1/dash/shops/3a31d58c-9273-4d74-bb78-2c55aad39453/products?categoryId=${categoryId}`);
 
     try {
         const response = await fetch(url, {
@@ -11,27 +10,16 @@ export default async function handler(req, res) {
                 Authorization: `Bearer ${process.env.NEXT_PUBLIC_BILLGANG_API_KEY}`
             }
         });
-
         if (!response.ok) {
-            console.error("Failed to fetch from Billgang API:", {
-                status: response.status,
-                statusText: response.statusText,
-                headers: [...response.headers],
-            });
+            console.error("Failed to fetch groups from Billgang:", response.status, response.statusText);
             res.status(response.status).json({ error: response.statusText });
             return;
         }
 
         const data = await response.json();
-        if (!data || !data.data) {
-            console.error("Unexpected response format from Billgang API:", data);
-            res.status(500).json({ error: "Unexpected response format" });
-            return;
-        }
-
         res.status(200).json(data.data);
     } catch (error) {
-        console.error("Error in API handler:", error);
-        res.status(500).json({ error: "Internal Server Error", details: error.message });
+        console.error("Error fetching group data:", error); // Log error details
+        res.status(500).json({ error: 'Error fetching group data' });
     }
 }

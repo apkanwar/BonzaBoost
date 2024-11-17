b738a769923236968764f380e9eed388a6dc8b57
Atinderpaul Kanwar <ap_kanwar@outlook.com>
2024-11-16 20:54:08 -0500

    fixing billing key issue

--- src/pages/index.js
+++ src/pages/index.js
@@ -34,7 +34,6 @@ export default function Home() {
       <Head>
         <title>Bonza Boost</title>
       </Head>
-      <GTM />
       <RoundedNavbar links />
       <InfoSection
         id="top"

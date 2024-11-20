6d4a1e71bbd8aa2f2ee5649e76d5f7b216c9396e
Atinderpaul Kanwar <ap_kanwar@outlook.com>
2024-11-20 15:49:50 -0500

    Center text

--- src/components/confetti.js
+++ src/components/confetti.js
@@ -47,7 +47,7 @@ export default function Confetti() {
         className="absolute top-0 left-0 w-full h-full pointer-events-none"
       />
       <div className="absolute flex flex-col items-center justify-center w-full h-full gap-4">
-        <h1 className="text-5xl font-bold text-white bg-opacity-50 px-4 py-2 rounded-md font-headings">
+        <h1 className="text-5xl text-center font-bold text-white bg-opacity-50 px-4 py-2 rounded-md font-headings">
           Congrats on your Successful Purchase!
         </h1>
         <Link href={'https://www.discord.gg/Bonza'} target='_blank' className="p-2 hover:bg-artic-blue rounded-md mx-2 cursor-pointer transition ease-in duration-200">

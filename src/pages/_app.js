d47808a2791644cfb58015bd3d54bbc902fbe2c5
Atinderpaul Kanwar <ap_kanwar@outlook.com>
2024-11-21 22:50:46 -0500

    Adding Tag

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,6 +4,19 @@ export default function App({ Component, pageProps }) {
 
   return (
     <main>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=AW-16778799212"
+      />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'AW-16778799212');
+        `}
+      </Script>
       <Component {...pageProps} />
     </main>
   )
